1270cfdbd150d31fe948129183df97cf7e1e2cd5
dadan_99r <dramdani159@gmail.com>
2020-04-28 05:44:02 +0700

    student folder

---
+++ resources/views/master/student/index.blade.php
@@ -0,0 +1,188 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Static Table Start -->
+<div class="data-table-area mg-b-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="sparkline13-list">
+                    <div class="sparkline13-hd">
+                        <div class="main-sparkline13-hd">
+                        <div class="container-sm">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h3>Data Siswa</h3>
+                                </div>
+                                <div class="col-md-6">
+                                    <a style="float:right" data-toggle="modal" href="#modalAdd" class="btn btn-success" ><i class="fa fa-plus"></i> Tambah </a>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="sparkline13-graph">
+                        <div class="datatable-dashv1-list custom-datatable-overright">
+                            <div id="toolbar">
+                                <select class="form-control dt-tb">
+                                    <option value="">Export Basic</option>
+                                    <option value="all">Export All</option>
+                                    <option value="selected">Export Selected</option>
+                                </select>
+                            </div>
+                            <table id="table" data-toggle="table" data-pagination="true" data-search="true" data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true" data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
+                                data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true" data-toolbar="#toolbar">
+                                <thead>
+                                    <tr>
+                                        <th data-field="state" data-checkbox="true"></th>
+                                        <th data-field="id">No</th>
+                                        <th data-field="name" data-editable="true">Nama Jurusan</th>
+                                        <th data-field="action">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($studens as $data)
+                                    <tr>
+                                        <td></td>
+                                        <td>{{$no++}}</td>
+                                        <td>{{$data->nama}}</td>
+                                        <td>
+                                            <a href="#" class="btn btn-info" onclick="editConfirm( '{{$data->id}}', '{{$data->nama}}')" title="Edit"><i class="fa fa-edit"> Edit</i></a>
+                                            <a href="{{ route('majors.destroy',$data) }}" class="btn btn-danger" onclick="event.preventDefault();destroy('{{ route('majors.destroy',$data) }}');" title="Hapus"><i class="fa fa-trash"></i> Hapus</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        <!-- Static Table End -->
+
+<!-- modal add -->
+<div class="modal fade bd-example-modal-lg" id="modalAdd" tabindex="-1" role="dialog" aria-labelledby="modalAddLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalAddLabel">Add New Salary</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="{{ route('majors.store') }}" role="form" method="post">
+          {{csrf_field()}}
+          <div class="form-group">
+            <label class="control-label col-md-2">Nama Jurusan</label>
+            <input name='nama' placeholder=" Masukan Nama Jurusan" type='text' class='form-control' required>
+          </div>
+            <br>
+          <button type='submit' class="btn btn-primary"><i class="fa fa-floppy-o"></i> Save</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- modal edit -->
+<div class="modal fade bd-example-modal-lg" id="modalUpdate" tabindex="-1" role="dialog" aria-labelledby="modalUpdateLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalUpdateLabel">Update Sale</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form id='editJurusan' role="form" method="post">
+        @method('PUT')
+          {{csrf_field()}}
+          <div class="form-group">
+          <div class="form-group">
+            <label class="control-label col-md-2">Nama Jurusan</label>
+            <input name='nama' id='nama' placeholder="Masukan Nama Jurusan" type='text' class='form-control' required>
+          </div>
+            <br>
+          <button type='submit' class="btn btn-primary"><i class="fa fa-floppy-o"></i> Save</button>
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- hapus -->
+<form id="destroy-form" method="POST">
+    @method('DELETE')
+    @csrf
+</form>
+@endsection
+
+@push('styles')
+<!-- x-editor CSS  -->
+    <link rel="stylesheet" href="{{ asset('assets/css/editor/select2.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/editor/datetimepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/editor/bootstrap-editable.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/editor/x-editor-style.css') }}">
+    <!-- normalize CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/css/data-table/bootstrap-table.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/data-table/bootstrap-editable.css') }}">
+@endpush
+
+@push('scripts')
+
+    <script>
+
+    function editConfirm(id,nama)
+    {
+        $('#nama').attr('value',nama);
+        $('#editJurusan').attr('action',"{{ url('majors') }}/"+id)
+        $('#modalUpdate').modal();
+    }
+
+    function destroy(action){
+        swal({
+            title: 'Apakah anda yakin?',
+            text: 'Setelah dihapus, Anda tidak akan dapat mengembalikan data ini!',
+            icon: 'warning',
+            buttons: ["Cancel", "Yes!"],
+        }).then(function(value) {
+            if (value) {
+            document.getElementById('destroy-form').setAttribute('action', action);
+            document.getElementById('destroy-form').submit();
+            }else {
+            swal("Data kamu aman!");
+        }
+        });
+    }
+    </script>
+    <!-- data table JS
+		============================================ -->
+    <script src="{{ asset('assets/js/data-table/bootstrap-table.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/tableExport.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/data-table-active.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/bootstrap-table-editable.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/bootstrap-editable.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/bootstrap-table-resizable.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/colResizable-1.5.source.js') }}"></script>
+    <script src="{{ asset('assets/js/data-table/bootstrap-table-export.js') }}"></script>
+    <!--  editable JS
+		============================================ -->
+    <script src="{{ asset('assets/js/editable/jquery.mockjax.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/mock-active.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/select2.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/moment.min.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/bootstrap-datetimepicker.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/bootstrap-editable.js') }}"></script>
+    <script src="{{ asset('assets/js/editable/xediable-active.js') }}"></script>
+@endpush

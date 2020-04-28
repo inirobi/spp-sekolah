a035ff96f5d2c8286f8125fd44a925031b87a7bc
Robi Gunawan <inirobi33@gmail.com>
2020-04-29 03:28:36 +0700

    fix bug table history

--- resources/views/master/financingcategory/index.blade.php
+++ resources/views/master/financingcategory/index.blade.php
@@ -194,7 +194,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal" onclick="$('#table_history tbody tr').remove();">Close</button>
             </div>
         </div>
     </div>

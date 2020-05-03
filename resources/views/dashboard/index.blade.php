60e2aa2d588f9276cad92b88837387656e48e4f1
Robi Gunawan <inirobi33@gmail.com>
2020-05-04 00:38:05 +0700

    import login yg lama

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -71,76 +71,6 @@ SPP | Dashboard
         </div>
     </div>
 </div>
-<!-- Static Table Start -->
-<div class="data-table-area mg-b-15 mg-t-15">
-    <div class="container-fluid" style="margin-top:20">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="sparkline13-list">
-                    <div class="sparkline13-hd">
-                        <div class="container-sm">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="main-sparkline13-hd">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="sparkline13-graph">
-                        <div class="datatable-dashv1-list custom-datatable-overright">
-                            <div id="toolbar">
-                                <select class="form-control dt-tb">
-                                    <option value="">Export Basic</option>
-                                    <option value="all">Export All</option>
-                                    <option value="selected">Export Selected</option>
-                                </select>
-                            </div>
-                            <table id="table" data-toggle="table" data-pagination="true" data-search="true"
-                                data-show-columns="true" data-show-pagination-switch="true" data-show-refresh="true"
-                                data-key-events="true" data-show-toggle="true" data-resizable="true" data-cookie="true"
-                                data-cookie-id-table="saveId" data-show-export="true" data-click-to-select="true"
-                                data-toolbar="#toolbar">
-                                <thead>
-                                    <tr>
-                                        <th data-field="state" data-checkbox="true"></th>
-                                        <th data-field="id">ID</th>
-                                        <th data-field="name" data-editable="false">Task</th>
-                                        <th data-field="email" data-editable="true">Email</th>
-                                        <th data-field="phone" data-editable="true">Phone</th>
-                                        <th data-field="complete">Completed</th>
-                                        <th data-field="task" data-editable="true">Task</th>
-                                        <th data-field="date" data-editable="true">Date</th>
-                                        <th data-field="price" data-editable="true">Price</th>
-                                        <th data-field="action">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td data-field="state" data-checkbox="true"></td>
-                                        <td data-field="id">ID</td>
-                                        <td data-field="name" data-editable="false">Task</td>
-                                        <td data-field="email" data-editable="true">Email</td>
-                                        <td data-field="phone" data-editable="true">Phone</td>
-                                        <td data-field="complete">Completed</td>
-                                        <td data-field="task" data-editable="true">Task</td>
-                                        <td data-field="date" data-editable="true">Date</td>
-                                        <td data-field="price" data-editable="true">Price</td>
-                                        <td data-field="action">Action</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Static Table End -->
 @endsection
 
 @push('styles')
@@ -174,4 +104,15 @@ SPP | Dashboard
 <script src="{{ asset('assets/js/editable/bootstrap-datetimepicker.js') }}"></script>
 <script src="{{ asset('assets/js/editable/bootstrap-editable.js') }}"></script>
 <script src="{{ asset('assets/js/editable/xediable-active.js') }}"></script>
+@endpush
+
+
+@push('breadcrumb-right')
+<div style="float:right">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb" style="margin-bottom:0">
+            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+        </ol>
+    </nav>
+</div>
 @endpush
